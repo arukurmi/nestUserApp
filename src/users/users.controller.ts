3ca6dcb3f4cee0627fd431ee3f9560f768cb6cd3
Aryansh Kurmi <arukurmi22@gmail.com>
2024-08-13 14:30:44 +0530

    add user routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,32 @@
-import { Controller, Delete, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { log } from 'console';
 
 @Controller('users')
 export class UsersController {
     @Get()
     getUsers() {
-        return 'hello world';
+        return [];
     }
 
-    @Delete()
-    deleteUser(){
-        
+    @Get(':id')
+    getUserInfo(@Query('id') id: string) {
+        console.log("alkrhasdlfhjsdkl");
+        console.log(id);
+        return {id};
+    }
+
+    @Patch(':id')
+    updateUser(@Param('id') id: string, @Body() updateUser: {}){
+        return id;
+    }
+
+    @Post()
+    createUser(@Body() user: {}){
+        return user;
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id: string){
+        return id;
     }
 }
